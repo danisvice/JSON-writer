85ea15dadd08c612fa8ac42275ccbb3963f0a319
Danny <d.ortixe@icloud.com>
2023-08-09 17:59:28 -0700

    Updated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Paragraph {
+    name: String
+}
+
+#[derive(Serialize, Deserialize)]
+struct Article {
+    article: String,
+    author: String,
+    paragraph: Vec<Paragraph>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let article: Article = Article {
+        article: String::from("How to work with JSON in Rust"),
+        author: String::from("Danny"),
+        paragraph: vec! [
+            Paragraph {
+                name: String::from("First Sentence")
+            },
+            Paragraph {
+                name: String::from("Body of the paragraph")
+            },
+            Paragraph {
+                name: String::from("End of the paragraph")
+            }
+        ]
+    };
+    let json = serde_json::to_string(&article).unwrap();
+    println!("The JSON is: {}", json)
 }
